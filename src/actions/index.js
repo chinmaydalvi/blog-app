46c71d8a63c157be5404e7737de6839064883560
chinmaydalvi <chinmaydalvi94@gmail.com>
2017-11-12 00:23:01 +0530

    added new post component

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?KEY=PAPERCLIP1234';
 
 
-export function fetchPosts() {
+export function fetchPosts(){
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
 	return{
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import reducers from './reducers';
 import promise from 'redux-promise';
 
 
+
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
+
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,7 +20,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
   	<BrowserRouter>
 	    <div>
-	    	<Route path="/" component={PostsIndex} />
+	    	<Switch>
+	    		<Route path="/posts/new" component={PostsNew} />
+	    		<Route path="/" component={PostsIndex} />
+	    	</Switch>
 	    </div>
     </BrowserRouter>
   </Provider>
